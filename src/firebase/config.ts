d3f91b09515130d0d08bf50471430de9098b9cc0
sidney.jr <sidney.e.s.s.jr@gmail.com>
2024-05-23 17:09:58 -0300

    feat: integra a plicação ao firebase

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,5 +1,4 @@
 // Import the functions you need from the SDKs you need
-import { getAnalytics } from "firebase/analytics";
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
@@ -9,16 +8,15 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBG6L1VTpoUmoL_5pv7DfJyy1RHeMSYnc4",
-  authDomain: "meus-15-anos-25-anos-depois.firebaseapp.com",
-  projectId: "meus-15-anos-25-anos-depois",
-  storageBucket: "meus-15-anos-25-anos-depois.appspot.com",
-  messagingSenderId: "419906390248",
-  appId: "1:419906390248:web:97315de31cce39a8d8a15d",
-  measurementId: "G-GDCZX35HWD",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
   privateKey: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY,
   clientEmail: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
-
 };
 
 // Initialize Firebase
