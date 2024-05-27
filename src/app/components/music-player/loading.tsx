48d9b58e351583e1fc1a4ce8ea754701f6999470
sidney.jr <sidney.e.s.s.jr@gmail.com>
2024-05-27 17:24:41 -0300

    feat: adiciona um loading skeleton aos componentes da home

--- src/app/components/music-player/loading.tsx
+++ src/app/components/music-player/loading.tsx
@@ -5,22 +5,38 @@ import { Box, Container, Skeleton, styled } from "@mui/material";
 const PlayerSkeletonComponent = styled(
   Container,
   {}
-)(({}) => ({
+)(({ theme }) => ({
   height: "73px",
   width: "100%",
   display: "flex",
   gap: ".5rem",
   padding: ".5rem",
+  "&>*:nth-child(1)": {
+    width: "40%",
+  },
+  "&>*:nth-child(2)": {
+    width: "60%",
+  },
+  [theme.breakpoints.down("sm")]: {
+    flexDirection: "column",
+    height: "133px",
+    "&>*:nth-child(1)": {
+      width: "80%",
+    },
+    "&>*:nth-child(2)": {
+      width: "100%",
+    },
+  },
 }));
 
 export default function Loading() {
   return (
     <PlayerSkeletonComponent>
-      <Box sx={{ width: "40%" }}>
+      <Box>
         <Skeleton variant="text" height="50%" />
         <Skeleton variant="text" height="50%" />
       </Box>
-      <Skeleton variant="rectangular" width="60%" height="100%" />
+      <Skeleton variant="rectangular" height="100%" />
     </PlayerSkeletonComponent>
   );
 }
